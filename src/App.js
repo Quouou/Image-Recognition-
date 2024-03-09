4ec4812a15a349c64b513661b8d7015669b94bba
Christopher Benedicto <benedicto.christopheeer@gmail.com>
2024-03-10 00:50:38 +0800

    backend impementation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Clarifai from 'clarifai';
+ import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
 import Signin from './components/Sigin/Signin';
@@ -63,8 +63,25 @@ class App extends Component {
       imageUrl:'',
       box:{},
       route:'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '', 
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: { 
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+  }})
   }
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
@@ -87,7 +104,22 @@ class App extends Component {
     this.setState({imageUrl: this.state.input})
     fetch("https://api.clarifai.com/v2/models/" + 'face-detection' +  "/outputs", getClarifai(this.state.input))
         .then(response => response.json())
-        .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
+        .then(result => {
+          if(result){
+            fetch('http://localhost:3000/image', {
+              method: 'put',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                  id: this.state.user.id,
+              })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user,{entries:count}))
+          })
+          }
+          this.displayFaceBox(this.calculateFaceLocation(result))
+        })
         .catch(error => console.log('error', error));
   }
 
@@ -112,17 +144,16 @@ class App extends Component {
         {this.state.route === 'home' 
         ?  <div>
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
               <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
               <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
           </div>
          
           : (
             this.state.route === 'signin' ?
-            <Signin onRouteChange={this.onRouteChange} /> 
-          : <Register onRouteChange={this.onRouteChange} /> 
+            <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
           )
-          
         }
       </div>
     );
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,18 +2,17 @@ import React from "react";
 
 
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
     return (
-        <div>
-            <div className="white f3">
-                {'Name, your current rank is'}
-            </div>
-            <div className="white f1">
-                {'#1'}
-            </div>
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
         </div>
-      
-    )
-}
+        <div className='white f1'>
+          {entries}
+        </div>
+      </div>
+    );
+  }
 
 export default Rank
